31979ae2e7cb4bb3b96a21d6af407d97f64441fb
Mohamed Saleh <mohamed20163858@gmail.com>
2022-08-19 01:39:22 +0200

    add functionality of adding new items

--- src/index.js
+++ src/index.js
@@ -11,3 +11,40 @@ import './style.css';
 // }
 
 // document.body.appendChild(component());
+let toDoListArray = [];
+let i = 0;
+const loadPageElements = () => {
+  const ul = document.querySelector('ul');
+  for (let i = 0; i < toDoListArray.length; i += 1) {
+    const li = document.createElement('li');
+    li.innerHTML = `
+    <div class="choices">
+        <input type="checkbox" name="item-${i + 1}" id="item-${i + 1}">
+        <label for="item-${i + 1}">${toDoListArray[i].description}</label>
+    </div>
+    <i class="fa-solid fa-ellipsis-vertical"></i>
+    `;
+    ul.appendChild(li);
+  }
+  const li = document.createElement('li');
+  li.innerHTML = '<li><a href="#">Clear all completed</a></li>';
+  ul.appendChild(li);
+};
+if (window.localStorage.tasks) {
+  toDoListArray = JSON.parse(window.localStorage.tasks);
+  loadPageElements();
+}
+const listInput = document.querySelector('#add-item');
+listInput.addEventListener('keyup', (event) => {
+  if (event.key === 'Enter') {
+    const object = {};
+    object.description = listInput.value;
+    object.completed = false;
+    object.index = i;
+    i += 1;
+    toDoListArray.push(object);
+    listInput.value = '';
+    window.localStorage.tasks = JSON.stringify(toDoListArray);
+    loadPageElements();
+  }
+});
