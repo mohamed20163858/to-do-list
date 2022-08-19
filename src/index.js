5c749141e3f016c8aab11d288e0acc2081581b13
Mohamed Saleh <mohamed20163858@gmail.com>
2022-08-19 21:50:11 +0200

    add add and remove tasks functions

--- src/index.js
+++ src/index.js
@@ -1,41 +1,93 @@
 import './style.css';
 
 let toDoListArray = [];
-let i = 0;
-const loadPageElements = () => {
-  const ul = document.querySelector('ul');
-  const liLast = document.querySelector('ul li:last-of-type');
-  liLast.remove();
-  for (let i = 0; i < toDoListArray.length; i += 1) {
-    const li = document.createElement('li');
-    li.innerHTML = `
-    <div class="choices">
-        <input type="checkbox" name="item-${i + 1}" id="item-${i + 1}">
-        <label for="item-${i + 1}">${toDoListArray[i].description}</label>
-    </div>
-    <i class="fa-solid fa-ellipsis-vertical"></i>
-    `;
-    ul.appendChild(li);
-  }
+const showElement = (i) => {
+  const ul = document.querySelector('.inner');
   const li = document.createElement('li');
-  li.innerHTML = '<li><a href="#">Clear all completed</a></li>';
+  li.classList.add('added-item');
+  li.innerHTML = `
+  <div class="choices">
+      <i class="fa-regular fa-square"></i>
+      <input type="text" name="item-${i + 1}-text" id="item-${i + 1}-text" class="list-item" value="${toDoListArray[i].description}">
+      <label for="item-${i + 1}-text"></label>
+  </div>
+  <i class="fa-solid fa-ellipsis-vertical"></i>
+  `;
   ul.appendChild(li);
+};
+
+const loadPageElements = () => {
+  for (let i = 0; i < toDoListArray.length; i += 1) {
+    showElement(i);
+  }
+};
+const addTask = () => {
+  const listInput = document.querySelector('#add-item');
+  const object = {};
+  object.description = listInput.value;
+  object.completed = false;
+  object.index = toDoListArray.length + 1;
+  toDoListArray.push(object);
+  listInput.value = '';
+  window.localStorage.tasks = JSON.stringify(toDoListArray);
+};
+const removeTask = (li, i) => {
+  toDoListArray.splice(i, 1);
+  li.remove();
+  for (let j = i; j < toDoListArray.length; j += 1) {
+    toDoListArray[j].index -= 1;
+  }
+  window.localStorage.tasks = JSON.stringify(toDoListArray);
+};
+const moveIntoListItemAction = (listItem) => {
+  const li = listItem.parentElement.parentElement;
+  li.addEventListener('click', () => {
+    const icon = listItem.parentElement.parentElement.querySelector('div+i');
+    icon.classList.remove('fa-ellipsis-vertical');
+    icon.classList.add('fa-trash');
+    listItem.parentElement.parentElement.style.backgroundColor = '#FFFECA';
+    listItem.style.backgroundColor = '#FFFECA';
+  });
+};
+const moveOutListItemAction = (listItem) => {
+  const li = listItem.parentElement.parentElement;
+  li.addEventListener('mouseleave', () => {
+    const icon = listItem.parentElement.parentElement.querySelector('div+i');
+    icon.classList.remove('fa-trash');
+    icon.classList.add('fa-ellipsis-vertical');
+    listItem.parentElement.parentElement.style.backgroundColor = 'white';
+    listItem.style.backgroundColor = 'white';
+  });
+};
+const deleteIconClickAction = (listItem, i) => {
+  const li = listItem.parentElement.parentElement;
+  const icon = li.querySelector('div+i');
+  icon.addEventListener('click', () => {
+    if (icon.classList.contains('fa-trash')) {
+      removeTask(li, i);
+    }
+  });
 };
 if (window.localStorage.tasks) {
   toDoListArray = JSON.parse(window.localStorage.tasks);
   loadPageElements();
 }
+
 const listInput = document.querySelector('#add-item');
 listInput.addEventListener('keyup', (event) => {
   if (event.key === 'Enter') {
-    const object = {};
-    object.description = listInput.value;
-    object.completed = false;
-    object.index = i;
-    i += 1;
-    toDoListArray.push(object);
-    listInput.value = '';
-    window.localStorage.tasks = JSON.stringify(toDoListArray);
-    loadPageElements();
+    addTask();
+    showElement(toDoListArray.length - 1);
+    const listItem = document.querySelector('.added-item:last-of-type .list-item');
+    moveIntoListItemAction(listItem);
+    moveOutListItemAction(listItem);
+    deleteIconClickAction(listItem, toDoListArray.length - 1);
   }
 });
+const listItems = document.querySelectorAll('.list-item');
+// const listCheckBoxes = document.querySelectorAll('.choices i');
+for (let i = 0; i < listItems.length; i += 1) {
+  moveIntoListItemAction(listItems[i]);
+  moveOutListItemAction(listItems[i]);
+  deleteIconClickAction(listItems[i], i);
+}
